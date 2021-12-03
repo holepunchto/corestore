e6fca750d8cc0f2f224cce3b2d7bae129c23450f
Andrew Osheroff <andrewosh@gmail.com>
2021-12-03 15:31:55 +0100

    Fix replication stream

--- index.js
+++ index.js
@@ -143,8 +143,8 @@ module.exports = class Corestore extends EventEmitter {
     return core
   }
 
-  replicate (opts = {}) {
-    const stream = isStream(opts) ? opts : (opts.stream || Hypercore.createProtocolStream(opts))
+  replicate (isInitiator, opts) {
+    const stream = Hypercore.createProtocolStream(isInitiator, opts)
     for (const core of this.cores.values()) {
       core.replicate(stream)
     }
@@ -224,10 +224,6 @@ function generateNamespace (first, second) {
   const out = Buffer.allocUnsafe(32)
   sodium.crypto_generichash(out, second ? Buffer.concat([first, second]) : first)
   return out
-}
-
-function isStream (s) {
-  return typeof s === 'object' && s && typeof s.pipe === 'function'
 }
 
 function noop () {}
