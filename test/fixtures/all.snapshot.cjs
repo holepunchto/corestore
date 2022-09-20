e3c1802a7a193ea524a2d62cfc91743139377add
Andrew Osheroff <andrewosh@gmail.com>
2022-09-20 10:28:07 +0200

    Add key generation snapshot test

---
+++ test/fixtures/all.snapshot.cjs
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+const { createTypedArray } = require('brittle')
+
+exports['generated namespaces/keys match fixtures - should match snapshot - 0'] = createTypedArray(Uint8Array, 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=')
+
+exports['generated namespaces/keys match fixtures - should match snapshot - 1'] = createTypedArray(Uint8Array, 'TCP+yR34ErI06KboHnFtmYJN9zXUxhIqqs01hMkOVZ0=')
+
+exports['generated namespaces/keys match fixtures - should match snapshot - 2'] = createTypedArray(Uint8Array, '0zl3JuYuq0EAU+P2etCDFYyHYF5bz1zdRXlcVYQ3J0A=')
+
+exports['generated namespaces/keys match fixtures - should match snapshot - 3'] = createTypedArray(Uint8Array, 'qLsVKDM6FYHS0Kvc1z0R/B0lpqUa6HnKMXkS/vYzKPk=')
+
+exports['generated namespaces/keys match fixtures - should match snapshot - 4'] = createTypedArray(Uint8Array, '61lauJ0wyfUB7XYW8BG7mdx7ommL69AM25sSxGwezgk=')
+
+exports['generated namespaces/keys match fixtures - should match snapshot - 5'] = createTypedArray(Uint8Array, 'vSvgW2MoO96VT5LHbqPwD2NdV6BhIS5LxAg6XgY9Ljw=')
+
+/* eslint-enable */
