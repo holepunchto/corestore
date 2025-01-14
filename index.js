e3fbf35307352ff9043376a3c85f4b5594aa2fb3
Christophe Diederichs <chm-diederichs@protonmail.com>
2025-01-14 10:18:18 +0000

    correctly set primary key if parallel

--- index.js
+++ index.js
@@ -202,7 +202,7 @@ class Corestore extends ReadyResource {
     }
 
     if (primaryKey === null) {
-      await this.storage.setSeed(this.primaryKey)
+      this.primaryKey = await this.storage.setSeed(this.primaryKey)
       return
     }
 
