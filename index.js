85a2fd2dca6804467cdf43cbea3f4a72a8a11156
Mathias Buus <mathiasbuus@gmail.com>
2025-09-15 18:25:00 +0200

    fwd readonly

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ class Corestore extends ReadyResource {
     super()
 
     this.root = opts.root || null
-    this.storage = this.root ? this.root.storage : Hypercore.defaultStorage(storage, { id: opts.id, allowBackup: opts.allowBackup })
+    this.storage = this.root ? this.root.storage : Hypercore.defaultStorage(storage, { id: opts.id, allowBackup: opts.allowBackup, readOnly: opts.readOnly })
     this.streamTracker = this.root ? this.root.streamTracker : new StreamTracker()
     this.cores = this.root ? this.root.cores : new CoreTracker()
     this.sessions = new SessionTracker()
